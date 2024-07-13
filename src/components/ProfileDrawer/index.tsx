72b049d74a477a10bbea937edc28afd47fefb9e0
Manvith <148960168+manvith2003@users.noreply.github.com>
2024-07-13 21:10:37 +0530

    add delete conversation

--- src/components/ProfileDrawer/index.tsx
+++ src/components/ProfileDrawer/index.tsx
@@ -1,155 +1,161 @@
-"use client"
+"use client";
 
-import React, { Fragment, useMemo } from 'react'
-import { IConversation } from '@/models/Conversation'
-import { IUser } from '@/models/User'
-import { format } from 'date-fns'
-import {Dialog, Transition} from "@headlessui/react"
-import {IoClose, IoTrash} from 'react-icons/io5'
-import { Avatar } from '@mui/material'
-import Avatara from '../Avatara'
+import React, { Fragment, useMemo, useState } from "react";
+import { IConversation } from "@/models/Conversation";
+import { IUser } from "@/models/User";
+import { format } from "date-fns";
+import { Dialog, Transition } from "@headlessui/react";
+import { IoClose, IoTrash } from "react-icons/io5";
+import { Avatar } from "@mui/material";
+import Avatara from "../Avatara";
+import Modal from "../Modal";
+import ComfirmModal from "../ComfirmModal";
 
-interface ProfileDrawerProps{
-    
-    otherUserName: string | null;
-    isOpen: boolean;
-    onClose: () => void
-    data:IConversation &{
-        users:IUser[]
-    }
+interface ProfileDrawerProps {
+  otherUserName: string | null;
+  isOpen: boolean;
+  onClose: () => void;
+  data: IConversation & {
+    users: IUser[];
+  };
 }
 
-const ProfileDrawer:React.FC<ProfileDrawerProps> = ({ data, otherUserName, isOpen, onClose }) => {
+const ProfileDrawer: React.FC<ProfileDrawerProps> = ({
+  data,
+  otherUserName,
+  isOpen,
+  onClose,
+}) => {
+  //    const joinedDate =useMemo(()=>{
+  //     return format(new Date(otherUserName.createdAt),'PP');
+  //    },[otherUserName.createdAt]);
 
+  const joinedDate = "May 28 2024";
 
-//    const joinedDate =useMemo(()=>{
-//     return format(new Date(otherUserName.createdAt),'PP');
-//    },[otherUserName.createdAt]);
+  const title = useMemo(() => {
+    return data.name || otherUserName;
+  }, [data.name, otherUserName]);
 
-const joinedDate='May 28 2024'
-
- const title = useMemo(()=>{
-   return data.name || otherUserName
-  
- },[data.name,otherUserName])
- 
-
- const statusText =useMemo(()=>{
-    if(data.isGroup){
-        return `${data.users.length} members`;
+  const statusText = useMemo(() => {
+    if (data.isGroup) {
+      return `${data.users.length} members`;
     }
-    return 'Active';
- },[data]);
+    return "Active";
+  }, [data]);
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <>
-    <Transition.Root  show={isOpen} as={Fragment}>
-     <Dialog as="div" className='z-50 relative' onClose={onClose}>
-        <Transition.Child as={Fragment} 
-        enter='ease-out duaration-500' 
-        enterFrom='opacity-0'
-         enterTo='opacity-100' 
-        leave='ease-in duration-500'
-          leaveFrom='opacity-100'
-          leaveTo='opacity-0'>
+      <ComfirmModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
 
-        <div className='fixed inset-0 bg-black bg-opacity-40'/>
+      <Transition.Root show={isOpen} as={Fragment}>
+        <Dialog as="div" className="z-50 relative" onClose={onClose}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duaration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-40" />
+          </Transition.Child>
+          <div className="fixed inset-0 overflow-hidden">
+            <div className="inset-0 absolut overflow-hidden">
+              <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+                <Transition.Child
+                  as={Fragment}
+                  enter="transform transition ease-in-out duration-500"
+                  enterFrom="translate-x-full"
+                  enterTo="translate-x-0"
+                  leave="transform transition ease-in-out duration-500"
+                  leaveTo="translate-x-full"
+                >
+                  <Dialog.Panel className="pointer-events-auto w-screen max-w-md ">
+                    <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl ">
+                      <div className="px-4 sm:px-6 ">
+                        <div className="flex items-start justify-end">
+                          <div className="ml-3 flex h-7 items-center">
+                            <button
+                              onClick={onClose}
+                              type="button"
+                              className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2"
+                            >
+                              <span className="sr-only">Close Panel</span>
+                              <IoClose size={24} />
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                      <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                        <div className="flex flex-col items-center">
+                          <div className="mb-2">
+                            <Avatara />
+                          </div>
+                          <div>{title}</div>
+                          <div className="text-sm text-gray-500">
+                            {statusText}
+                          </div>
+                          <div className="flex gap-10 my-8 ">
+                            <div
+                              onClick={() => setIsModalOpen(true)}
+                              className="flex flex-col gap-3 items-center cursor-pointer hover:opacity-75"
+                            >
+                              <div className="w-10 h-10 bg-blue-300 rounded-full flex items-center justify-center">
+                                <IoTrash size={20} />
+                              </div>
+                              <div className="text-sm font-light text-neutral-600 ">
+                                Delete
+                              </div>
+                            </div>
+                          </div>
+                          <div className="w-full pb-5 pt-5 sm:px-0 sm:pt-0">
+                            <dl className="space-y-8 px-4 sm:space-y-6 sm:px-6">
+                              {!data.isGroup && (
+                                <div>
+                                  <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">
+                                    Email
+                                  </dt>
+                                  <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
+                                    gunj@gmail.com
+                                  </dd>
+                                </div>
+                              )}
 
-        </Transition.Child>
-        <div className='fixed inset-0 overflow-hidden'>
-<div className='inset-0 absolut overflow-hidden'>
-    <div className='pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10'>
-     <Transition.Child as={Fragment} 
-     enter='transform transition ease-in-out duration-500'
-     enterFrom='translate-x-full'
-     enterTo='translate-x-0'
-     leave='transform transition ease-in-out duration-500'
-     leaveTo='translate-x-full'
-     >
-      <Dialog.Panel
-       className="pointer-events-auto w-screen max-w-md "
-       >
-        <div className='flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl '>
-            <div className='px-4 sm:px-6 '>
-              <div className='flex items-start justify-end'>
-                <div className='ml-3 flex h-7 items-center'>
-               <button onClick={onClose}  type='button' className='rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-2'>
-                 <span className='sr-only'>Close Panel</span>
-                 <IoClose size={24}/>
-               </button>
-                </div>
-
+                              {!data.isGroup && (
+                                <>
+                                  <hr />
+                                  <div>
+                                    <dt className="text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0">
+                                      Joined
+                                    </dt>
+                                    <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 ">
+                                      <time dateTime={joinedDate}>
+                                        {joinedDate}
+                                      </time>
+                                    </dd>
+                                  </div>
+                                </>
+                              )}
+                            </dl>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
               </div>
             </div>
-    <div className='relative mt-6 flex-1 px-4 sm:px-6'>
- <div className='flex flex-col items-center'>
-  <div className='mb-2'>
-    <Avatara/> 
-  </div>
-  <div>{title}</div>
-  <div className='text-sm text-gray-500'>
-        {statusText}
-  </div>
-  <div className='flex gap-10 my-8 '>
-    <div onClick={()=>{}} className='flex flex-col gap-3 items-center cursor-pointer hover:opacity-75'>
-     <div className='w-10 h-10 bg-blue-300 rounded-full flex items-center justify-center'>
-     <IoTrash size={20}/>
-     </div>
-     <div className='text-sm font-light text-neutral-600 '>
-        Delete
-     </div>
-    </div>
-  </div>
-  <div className='w-full pb-5 pt-5 sm:px-0 sm:pt-0'>
-  <dl className='space-y-8 px-4 sm:space-y-6 sm:px-6' >
-     {!data.isGroup && (
-        <div>
-            <dt className='text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0'>
-                 Email
-            </dt>
-            <dd className='mt-1 text-sm text-gray-900 sm:col-span-2'>
-                gunj@gmail.com
-            </dd>
-
-        </div>
-     )}
-
-     { !data.isGroup && (
-        <>
-        <hr/>
-        <div>
-            <dt className='text-sm font-medium text-gray-500 sm:w-40 sm:flex-shrink-0'>
-            Joined 
-            </dt>
-            <dd className='mt-1 text-sm text-gray-900 sm:col-span-2 '>
-             <time dateTime={joinedDate}>
-                  {joinedDate}
-             </time>
-            </dd>
-        </div>
-      
-        </>
-     )
-
-     }
-
-  </dl>
-  </div>
- </div>
-    </div>
-        </div>
-
-      </Dialog.Panel>
-     </Transition.Child>
-    </div>
-</div>
-        </div>
-     </Dialog>
-   </Transition.Root>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </>
-   
-  )
-}
+  );
+};
 
-export default ProfileDrawer
+export default ProfileDrawer;
